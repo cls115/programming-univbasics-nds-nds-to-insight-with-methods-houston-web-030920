84f0a5cfc8cd76a478af4a2de8da051fef477fc6
Colton stephens <stephens.colton@icloud.com>
2020-02-27 22:07:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,8 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  data = Hash.new []  
+  data = Hash.new [] 
+  data[:]
 binding.pry
 
 end
