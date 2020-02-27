cdcc8dd1dfa745e169ae7b719eeb74b91a4acc67
Colton stephens <stephens.colton@icloud.com>
2020-02-27 22:15:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,8 +6,9 @@ require 'directors_database'
 # using director_data as input
 def gross_for_director(director_data)
   data = Hash.new [] 
-  data[director_data[:name]]=0 
-  while
+  data[director_data[:name]]=0
+  i=0 
+  while 
   
 binding.pry
 
